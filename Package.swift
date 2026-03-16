fc3c658b5e68dbc2b86f9d66e17701183115e2a9
Portolabot <portolabot@hiportola.com>
2026-03-16 06:33:41 +0000

    Update to 5.5.0

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ISAppLovinAdapter",
-            url: "https://github.com/portolans/applovin-ironsourceadapter-releases/releases/download/5.4.0/ISAppLovinAdapter.xcframework.zip",
-            checksum: "e9172dbd7b213265aa9d8ba94f1441b4b01ff5379aea7b2bd3721370a94886f9",
+            url: "https://github.com/portolans/applovin-ironsourceadapter-releases/releases/download/5.5.0/ISAppLovinAdapter.xcframework.zip",
+            checksum: "3411cc9172f5c56391a2fb4c49e1ce31e7b0c8d9aa18917e8b340724e8227ef6",
         ),
     ],
 )
